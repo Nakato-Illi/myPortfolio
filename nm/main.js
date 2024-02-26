cfa35c3ff83484dfcf9c368ef68d9930ab7db992
Nakato-Illi <illi-nakato@hotmail.de>
2024-02-26 16:04:14 +0100

    email send successfully!

--- nm/main.js
+++ nm/main.js
@@ -1,5 +1,9 @@
-function clearForm() {
-    document.getElementById('nameInput').value = '';
-    document.getElementById('emailInput').value = '';
-    document.getElementById('messageInput').value = '';
+function sendMail() {
+    let parms = {
+        from_name : document.getElementById('from_name').value,
+        from_email : document.getElementById('from_email').value,
+        from_message : document.getElementById('from_message').value,
+    }
+
+    emailjs.send('service_jbjuudp', 'template_d0tfvrb', parms).then(alert('Email has been send successfully!'));
 }
